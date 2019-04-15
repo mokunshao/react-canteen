b09b5ef81b559d1ae4b9ad7486faa469354c7fc5
mokunshao <1627510957@qq.com>
2019-04-16 01:40:09 +0800

    备份

--- src/pages/About/Delivery.tsx
+++ src/pages/About/Delivery.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Delivery() {
   return (
     <div>
-      kuaidi
+      Delivery
     </div>
   )
 }
--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
 import { Tabs } from 'antd';
 import styles from './index.css';
+import { Switch, Route } from 'dva/router';
+import History from './History';
+import Contact from './Contact';
+import Docs from './Docs';
+import Delivery from './Delivery';
 
-export default function About() {
+interface Props {
+  history: {
+    push: any;
+  };
+  location: {
+    pathname: any;
+  };
+}
+
+export default function About(props: Props) {
+  function handleOnChange(key: string): void {
+    props.history.push(key);
+  }
   return (
     <div className={styles.about}>
-      <Tabs tabPosition="left" className={styles.tabs}>
-        <Tabs.TabPane tab={'历史订餐'} key={'历史订餐'}>
-          1
-        </Tabs.TabPane>
-        <Tabs.TabPane tab={'联系我们'} key={'联系我们'}>
-          2
-        </Tabs.TabPane>
-        <Tabs.TabPane tab={'点餐文档'} key={'点餐文档'}>
-          3
-        </Tabs.TabPane>
-        <Tabs.TabPane tab={'快递信息'} key={'快递信息'}>
-          3
-        </Tabs.TabPane>
+      <Tabs
+        tabPosition="left"
+        className={styles.tabs}
+        onChange={handleOnChange}
+        activeKey={props.location.pathname}
+      >
+        <Tabs.TabPane tab={'历史订餐'} key={'/about/history'} />
+        <Tabs.TabPane tab={'联系我们'} key={'/about/contact'} />
+        <Tabs.TabPane tab={'点餐文档'} key={'/about/docs'} />
+        <Tabs.TabPane tab={'快递信息'} key={'/about/delivery'} />
       </Tabs>
-      <div>
-        nihao
+      <div className={styles.content}>
+        <Switch>
+          <Route path="/about/history" component={History} />
+          <Route path="/about/contact" component={Contact} />
+          <Route path="/about/docs" component={Docs} />
+          <Route path="/about/delivery" component={Delivery} />
+        </Switch>
       </div>
     </div>
   );
