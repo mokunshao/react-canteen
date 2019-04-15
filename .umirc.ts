8c14de5cfd8f2a2c08f35964d885f8f294abe06a
mokunshao <1627510957@qq.com>
2019-04-15 20:50:53 +0800

    Update .umirc.ts

--- .umirc.ts
+++ .umirc.ts
@@ -1,28 +1,46 @@
 import { IConfig } from 'umi-types';
 
 // ref: https://umijs.org/config/
-const config: IConfig =  {
+const config: IConfig = {
   treeShaking: true,
   history: 'hash',
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: true,
-      dva: true,
-      dynamicImport: false,
-      title: 'react-canteen',
-      dll: false,
-      routes: {
-        exclude: [
-          /models\//,
-          /services\//,
-          /model\.(t|j)sx?$/,
-          /service\.(t|j)sx?$/,
-          /components\//,
-        ],
+    [
+      'umi-plugin-react',
+      {
+        antd: true,
+        dva: true,
+        dynamicImport: false,
+        title: 'react-canteen',
+        dll: false,
+        routes: {
+          exclude: [
+            /models\//,
+            /services\//,
+            /model\.(t|j)sx?$/,
+            /service\.(t|j)sx?$/,
+            /components\//,
+          ],
+        },
       },
-    }],
+    ],
   ],
-}
+  routes: [
+    {
+      path: '/',
+      component: '../layouts',
+      routes: [
+        { path: '/', redirect: '/home' },
+        { path: '/home', component: './Home' },
+        { path: '/menu', component: './Menu' },
+        { path: './admin', component: './Admin' },
+        { path: './about', component: './About' },
+        { path: './register', component: './Register' },
+        { path: './login', component: './Login' },
+      ],
+    },
+  ],
+};
 
 export default config;
